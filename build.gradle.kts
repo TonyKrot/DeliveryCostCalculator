7ef0eeb26547e57e35cd5b3498abda41e651e58d
TonyKrot <avkrotkevich@gmail.com>
2024-04-01 22:47:07 +0300

    Selenium BasicTestsFirstCommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation ("io.qameta.allure:allure-testng:2.14.0")
+    implementation ("org.seleniumhq.selenium:selenium-java:4.18.1")
+    testImplementation ("org.assertj:assertj-core:3.25.3")
     }
 
 tasks.test {
